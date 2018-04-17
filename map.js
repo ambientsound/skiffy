867e4a034bb63355dcd9603b4776d572d850b075
Kim Tore Jensen <kim@incendio.no>
2018-04-17 14:40:14 +0200

    Use markers instead of circles.

--- map.js
+++ map.js
@@ -3,10 +3,10 @@
 var oslo = {lat: 59.91588, lng: 10.7674953};
 
 var parks = {
-	//vesletjern: {lat: 59.9590291, lng: 10.8634601},
-	holmenkollen: {lat: 59.9640583, lng: 10.6675896},
-	sognsvann: {lat: 59.9701258, lng: 10.7305066},
-	skullerud: {lat: 59.8628795, lng: 10.8475724}
+	Vesletjern: {lat: 59.960181, lng: 10.861639},
+	Holmenkollen: {lat: 59.9640583, lng: 10.6675896},
+	Sognsvann: {lat: 59.9701258, lng: 10.7305066},
+	Skullerud: {lat: 59.8628795, lng: 10.8475724}
 };
 
 var overlay;
@@ -79,23 +79,19 @@ function initMap() {
 	};
 
 	var map = new google.maps.Map(document.getElementById('map'), {
-		zoom: 11,
-		center: oslo,
-		styles: style,
+		zoom: 11
+		,center: oslo
+		//,styles: style
 	});
 
 	for (var park in parks) {
-		// Add the circle for this city to the map.
-		var cityCircle = new google.maps.Circle({
-			strokeColor: '#FF0000',
-			strokeOpacity: 0.8,
-			strokeWeight: 2,
-			fillColor: '#FF0000',
-			fillOpacity: 0.35,
+		var marker = new google.maps.Marker({
+			position: parks[park],
 			map: map,
-			center: parks[park],
-			radius: 300,
+			title: park
 		});
+        marker.addListener('click', function() {
+        });
 	}
 
 	var bounds = new google.maps.LatLngBounds(
